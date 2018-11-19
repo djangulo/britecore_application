125d822900ae58a742ad783da54e014605f09164
djangulo <denis.angulo@gmail.com>
2018-11-19 13:48:30 -0400

    deploy.sh changes, yet to be tested

--- deploy.sh
+++ deploy.sh
@@ -17,26 +17,29 @@ echo $SQL_HOST
 sed "s@CHANGESECRETKEY@$SECRET_KEY@g" deploy/zappa_settings_template \
     | sed "s@DBPASS@$SQL_PASSWORD@g" \
     | sed "s@DBHOST@$SQL_HOST@g" \
-    > zappa_settings.json
+    > backend/zappa_settings.json
 
+cd backend
 pipenv run zappa deploy prod
 API_ADDRESS=$(pipenv run zappa status prod \
     | grep API \
     | sed "s@: @\n@g" \
     | grep http \
     | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+cd ..
 # add the API root of the endpoint created by Zappa into the
 # vue.js app
 # NOTE it does not have a trailing slash
-echo "export const apiRoot = '$API_ADDRESS/api/v1.0'" \
-    > frontend/src/store/modules/apiData.js
+echo "const liveAPI = '$API_ADDRESS/api/v1.0'" \
+    > frontend/src/store/modules/actual-api-root.js
+echo "export default liveAPI" >> frontend/src/store/modules/actual-api-root.js
 # cd to vue dir, set env, npm install, build, deploy
 cd frontend
 NODE_ENV=production
 npm install
 npm run build
 npm run deploy
-cd ..
+cd ../backend
 # migrate DB
 pipenv run zappa manage prod migrate
 # create initial samples
